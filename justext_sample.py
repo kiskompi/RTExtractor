c08bc18881baf03924a3d429c4ac87ca739fb7dc
kiskompi <kiskompi@gmail.com>
2017-03-06 17:17:40 +0100

    Működési folyamat bővítve, Implementáció folyamata berakva

--- justext_sample.py
+++
@@ -1,8 +0,0 @@
-import requests
-import justext
-
-response = requests.get("http://planet.python.org/")
-paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
-for paragraph in paragraphs:
-    if not paragraph.is_boilerplate:
-        print paragraph.text
